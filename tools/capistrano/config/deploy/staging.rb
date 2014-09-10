cda175a17f628ef203ddf7b5b6cea507c8fbc2f5
Manel R. Doménech <manel@manel.io>
2014-09-10 23:59:22 +0200

    reorganize directories

---
+++ tools/capistrano/config/deploy/staging.rb
@@ -0,0 +1,7 @@
+server "dev.memocracia.com", :app, :web, :primary => true
+
+set :deploy_to, "/var/www/#{domain}"
+set :deploy_via, :remote_cache
+
+set :user,        "#{application}"
+set :branch,      "master"
--- tools/compass/config.rb
+++ tools/compass/config.rb
@@ -3,8 +3,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "web/css"
-sass_dir = "sass"
+css_dir = "../../web/css"
+sass_dir = "../../sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
